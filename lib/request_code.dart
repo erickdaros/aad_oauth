7fe793e629e408b70d3a3b458f50044b27e73303
Erick Daros Silva <erickdarosilva@gmail.com>
2020-05-04 15:25:39 -0300

    test new setting

--- lib/request_code.dart
+++ lib/request_code.dart
@@ -33,7 +33,7 @@ class RequestCode {
         Uri.encodeFull("${_authorizationRequest.url}?$urlParams"),
         clearCookies: _authorizationRequest.clearCookies, 
         hidden: false,  
-        userAgent: getUserAgent(),
+        //userAgent: getUserAgent(),
         rect: _config.screenSize
     );
 
