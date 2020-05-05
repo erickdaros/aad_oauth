78e532574395a06a20b4d0f0a912a1f2072e7a3b
Erick Daros Silva <erickdarosilva@gmail.com>
2020-05-05 15:37:20 -0300

    fix b2c login on ios

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -19,23 +19,23 @@ class Config {
   String userAgent;
   String tokenIdentifier;
 
-  Config(
-      this.azureTenantId,
-      this.clientId,
-      this.scope,
-      this.redirectUri, {
-        this.isB2C = false,
-        this.nonce = "12345",
-        this.azureTenantName,
-        this.userFlow,
-        this.clientSecret,
-        this.resource,
-        this.responseType = "code",
-        this.contentType = "application/x-www-form-urlencoded",
-        this.screenSize,
-        this.userAgent,
-        this.tokenIdentifier = "Token",
-      }) {
+  Config({
+    this.azureTenantId,
+    this.clientId,
+    this.scope,
+    this.redirectUri,
+    this.isB2C = false,
+    this.nonce = "12345",
+    this.azureTenantName,
+    this.userFlow,
+    this.clientSecret,
+    this.resource,
+    this.responseType = "code",
+    this.contentType = "application/x-www-form-urlencoded",
+    this.screenSize,
+    this.userAgent,
+    this.tokenIdentifier = "Token",
+  }) {
     this.authorizationUrl = isB2C
         ? "https://$azureTenantName.b2clogin.com/$azureTenantName.onmicrosoft.com/$userFlow/oauth2/v2.0/authorize"
         : "https://login.microsoftonline.com/$azureTenantId/oauth2/v2.0/authorize";
